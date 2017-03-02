c93c5d7cfb58243229d2e588e06b6d1d496f156c
Maxime Bret <maxime.bret.ext@boursorama.fr>
2017-03-02 18:07:32 +0100

    no message

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-module.exports = {
+/**
+ * This is the plugin
+ */
+class Plugin {
 
     /**
-     *
-     * @param helper
-     * @param done
+     * @param {Object} chewie - Chewie instance
+     * @param {Object} helper - This helper provide some convenient methods that are often used
+     * in plugins. Ex: logger with your namespace set automatically.
      */
-    mount: function(helper, done) {
+    constructor(chewie, helper) {
+        console.log("ici");
+        // as the plugin instance is passed to each modules it could be useful to
+        // keep access to chewie instance.
+        this._chewie = chewie;
+        // like chewie, it may be useful to have access to helper inside the modules
         this.helper = helper;
-        return done();
-    },
+    }
 
     /**
-     *
-     * @param done
-     * @returns {*}
+     * Mount function is called everytime the plugin is mount.
+     * Note that a plugin cannot be mounted more than once in the same time.
+     * As long as you are not calling the callback function the plugin will not be
+     * fully mounted and module for example will not be loaded. Do not forget it!
+     * @param {Function} done
      */
-    unmount: function(done) {
-        return done();
+    mount(done) {
+        done();
     }
-};
+
+    /**
+     * This function is called everytime your plugin is unmount and have to shutdown.
+     * As long as you are not calling the callback function the plugin wait to be stopped. Do not forget it!
+     * @param {Function} done
+     */
+    unmount(done) {
+        done();
+    }
+
+    get chewie() {
+        return this._chewie;
+    }
+}
+
+module.exports = Plugin;
